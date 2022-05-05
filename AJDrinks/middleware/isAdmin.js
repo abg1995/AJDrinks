12b5aebe7459f04948328e8e259262719537b028
Jawid <41361157+infjay@users.noreply.github.com>
2022-05-05 14:27:10 +0200

    admin middle ware +

--- AJDrinks/middleware/isAdmin.js
+++ AJDrinks/middleware/isAdmin.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
     // checks if the user is logged in when trying to access a specific page
-    if (!req.session.user.admin) {
+    if (!req.session.admin) {
       return res.send("You Dont Have Permission");
       
     }
-    req.user = req.session.user;
+    req.admin = req.session.admin;
     next();
   };
