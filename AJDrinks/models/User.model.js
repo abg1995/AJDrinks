d24120c64135437e26237ce7260362ce2dce95ef
Alejandro blanco <56705985+abg1995@users.noreply.github.com>
2025-10-15 19:11:58 +0300

    no actual changes

--- AJDrinks/models/User.model.js
+++ AJDrinks/models/User.model.js
@@ -3,25 +3,23 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    clar: {
       type: String,
       unique: true,
       required: true,
-      trim: true
+      trim: true,
     },
     password: String,
     admin: {
       type: Boolean,
       default: false,
-      }
+    },
   },
   {
     timestamps: true,
-  },
-  
+  }
 );
 
 const User = model("User", userSchema);
 
 module.exports = User;
- 
