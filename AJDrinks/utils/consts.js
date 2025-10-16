e082144ac084bc8b96645909190dfd207765821f
Alejandro blanco <56705985+abg1995@users.noreply.github.com>
2025-10-16 17:00:50 +0300

    changes on const js mongo_uri for updated DB

--- AJDrinks/utils/consts.js
+++ AJDrinks/utils/consts.js
@@ -1,5 +1,3 @@
-const MONGO_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://alejandroblancog1995_db_user:1234test@ajdrinks.oc9jpsn.mongodb.net/?retryWrites=true&w=majority&appName=AJDrinks";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/ajdrinksDB";
 
 module.exports = MONGO_URI;
